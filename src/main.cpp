387d7573f9abc997c82d4b331173b3fbe7ba61af
Adrian <aprpkp@gmail.com>
2025-11-27 09:48:22 -0500

    added launch.json

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,18 @@
 
 int main()
 {
+    Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "raylib Tetris");
+    SetTargetFPS(60);
 
     while(WindowShouldClose() == false)
     {
-
+        BeginDrawing();
+        ClearBackground(darkBlue);
+        EndDrawing();
     }
 
     CloseWindow();
-    
+
 }
+
